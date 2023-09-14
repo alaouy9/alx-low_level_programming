2b367966f3c3a03ff84e0c08d05b32c93e88b53e
khalid <alaouykhalid9@gmail.com>
2023-09-14 15:01:26 -0700

    vvh

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,26 +1,30 @@
-carrie@ubuntu:/debugging$ cat 1-main.c
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
+* main - Entry point of the program
+*
+* Description: This function prints a message to indicate
+* that an infinite loop is avoided.
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-        int i;
+    int i;
 
-        printf("Infinite loop incoming :(\n");
+    printf("Infinite loop incoming :(\n");
 
-        i = 0;
+    i = 0;
 
-	// while (i < 10)
-	// {
-	// putchar(i);
-		// }
+    /*
+     * Commented out the while loop to prevent an infinite loop
+     * while (i < 10)
+     * {
+     *     putchar(i);
+     * }
+     */
 
-        printf("Infinite loop avoided! \\o/\n");
+    printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+    return (0);
 }
-carrie@ubuntu:/debugging$
