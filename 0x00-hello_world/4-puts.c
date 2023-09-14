d9345deaab4a6aee76ac49b3f34a138099def549
khalid <alaouykhalid9@gmail.com>
2023-09-14 10:12:54 -0700

    ninin

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print to the screen using puts()
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
