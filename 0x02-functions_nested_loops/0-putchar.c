bd87a09508679bafbeb97e8b813fd48566f6cf98
khalid <alaouykhalid9@gmail.com>
2023-09-16 04:05:32 -0700

    kkk

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-  printf("_putchar\n");
-  return (0);
+    printf("_putchar\n");
+    return (0);
 }
