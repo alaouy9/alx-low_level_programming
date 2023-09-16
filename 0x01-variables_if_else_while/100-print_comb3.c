f04f6f0c2454717cef89d8340b91b696d297de1d
khalid <alaouykhalid9@gmail.com>
2023-09-16 03:15:05 -0700

    ggg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,18 +7,23 @@
  */
 int main(void)
 {
-    int digit1, digit2;
+    int digit1, digit2, digit3;
 
     for (digit1 = 0; digit1 <= 8; digit1++)
     {
         for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
         {
-            putchar((digit1 % 10) + '0');
-            putchar((digit2 % 10) + '0');
-            if (digit1 != 8 || digit2 != 9)
+            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
             {
-                putchar(',');
-                putchar(' ');
+                putchar(digit1 + '0');
+                putchar(digit2 + '0');
+                putchar(digit3 + '0');
+
+                if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
             }
         }
     }
