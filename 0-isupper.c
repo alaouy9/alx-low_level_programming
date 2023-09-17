1fae8781901d4adb44f150a37feb70fe78b4f197
khalid <alaouykhalid9@gmail.com>
2023-09-17 03:55:29 -0700

    isuper function

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+/**
+* _isupper - check if a letter is uppercase
+*
+* @c: letter to check
+*
+* Return: 1 if the letter is uppercase 0 if not
+*/
+
+int _isuper(int c)
+
+{
+    if (c >= A && c <= Z)
+        return(1);
+        else {
+          return(0);
+    }
+
+}
